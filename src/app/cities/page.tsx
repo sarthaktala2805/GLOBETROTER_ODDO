66a89413aa504317259b0cd65caec7571c2ad07d
sarthaktala2805 <sarthaktala2805@gmail.com>
2026-09-18 11:22:19 +0530

    Update page.tsx

--- src/app/cities/page.tsx
+++ src/app/cities/page.tsx
@@ -1,5 +1,14 @@
 import ExplorePage from "@/app/explore/page";
 
+export async function generateStaticParams() {
+  return [
+    { city: "delhi" },
+    { city: "mumbai" },
+    { city: "jaipur" },
+    { city: "ahmedabad" },
+  ];
+}
+
 export default function CitiesPage() {
   return <ExplorePage />;
 }
